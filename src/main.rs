825142f067299cb6424471a9bcf1cc01291e26b0
James Schinner <james.peter.schinner@gmail.com>
2020-11-23 12:35:52 +1100

    Made changes as per clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,7 @@ static BASE10_NUMERALS: [&str; 7] = ["I", "X", "C", "M", "X̄", "C̄", "M̄"];
 
 static CENTRE_NUMERALS: [&str; 6] = ["V", "L", "D", "V̄", "L̄", "D̄"];
 
-struct Bases {
-    base: usize
-}
-
-
-impl Iterator for Bases {
-    type Item = usize;
-
-    fn next(&mut self) -> Option<usize> {
-        self.base = self.base - 1;
-        Some(self.base)
-    }
-}
-
-fn iter_bases(largest_base: usize) -> Bases {
-    Bases { base: largest_base }
-}
-
-
-fn encode((decimal_number, base, ): (char, usize, )) -> String {
+fn encode((decimal_number, base): (char, usize)) -> String {
     let digit = decimal_number.to_digit(10).unwrap();
     let max_base = CENTRE_NUMERALS.len();
 
@@ -34,35 +15,37 @@ fn encode((decimal_number, base, ): (char, usize, )) -> String {
         BASE10_NUMERALS[BASE10_NUMERALS.len() - 1]
             //This pow function is the main limiter for decimal size
             .repeat((10_u32.pow((base - max_base) as u32) * digit) as usize)
+    } else if digit == 9 {
+        format!(
+            "{}{}",
+            if base == 3 {
+                "Ī"
+            } else {
+                BASE10_NUMERALS[base]
+            },
+            BASE10_NUMERALS[base + 1]
+        )
+    } else if digit >= 5 {
+        format!(
+            "{}{}",
+            CENTRE_NUMERALS[base],
+            BASE10_NUMERALS[base].repeat((digit - 5) as usize)
+        )
+    } else if digit == 4 {
+        format!("{}{}", BASE10_NUMERALS[base], CENTRE_NUMERALS[base])
     } else {
-        if digit == 9 {
-            format!("{}{}"
-                    , if base == 3 { "Ī"} else { BASE10_NUMERALS[base] }
-                    , BASE10_NUMERALS[base + 1]
-            )
-        } else if digit >= 5 {
-            format!("{}{}", CENTRE_NUMERALS[
-                base], BASE10_NUMERALS[base]
-                        .repeat((digit - 5) as usize))
-        } else if digit == 4 {
-            format!("{}{}", BASE10_NUMERALS[base], CENTRE_NUMERALS[base])
-        } else {
-            // Less than 4
-            BASE10_NUMERALS[base].repeat(digit as usize)
-        }
+        // Less than 4
+        BASE10_NUMERALS[base].repeat(digit as usize)
     }
 }
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut writer = BufWriter::new(io::stdout());
-    let decimal_encoded_string = &args[1];
-    for roman_numeral in decimal_encoded_string
-        .chars()
-        .zip(iter_bases(decimal_encoded_string.len()))
-        .map(encode) {
-        writer.write(roman_numeral.as_bytes())
+    let input = &args[1];
+    for roman_numeral in input.chars().zip((0..input.len()).rev()).map(encode) {
+        writer
+            .write_all(roman_numeral.as_bytes())
             .expect("Unable to write to stdout");
     }
 }
@@ -109,5 +92,3 @@ mod tests {
         assert_eq!(encode(('9', 3)), "ĪX̄");
     }
 }
-
-
